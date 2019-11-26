c78d12eaa79716724be3e5ce306597b73fbdb081
Ravi <raven619claw@gmail.com>
2019-11-26 21:11:09 +0530

    fixed linter

--- src/client/App/index.js
+++ src/client/App/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+// eslint-disable-next-line react/prefer-stateless-function
 export default class App extends Component {
   render() {
     return <div>React</div>;
