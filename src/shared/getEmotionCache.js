4516f07922474acafa57c39663955cc9c70b0b51
Ravi <raven619claw@gmail.com>
2019-12-25 20:49:45 +0530

    fix emotion cache issue

--- src/shared/getEmotionCache.js
+++ src/shared/getEmotionCache.js
@@ -1,12 +1,14 @@
 import stylisRTL from 'stylis-rtl'
 import createCache from '@emotion/cache'
 
-export const myCacheRTL = createCache({
-  key: 'c',
-  stylisPlugins: [stylisRTL]
-})
-export const myCacheLTR = createCache({
-  key: 'c'
-})
+export const myCacheRTL = () =>
+  createCache({
+    key: 'c',
+    stylisPlugins: [stylisRTL]
+  })
+export const myCacheLTR = () =>
+  createCache({
+    key: 'c'
+  })
 
-export default isRTL => (isRTL ? myCacheRTL : myCacheLTR)
+export default isRTL => (isRTL ? myCacheRTL() : myCacheLTR())
