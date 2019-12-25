00ec2e1b4ef5783d1e463d328cb0f7598fb24c73
Ravi <raven619claw@gmail.com>
2019-12-25 21:50:16 +0530

    fix babel

--- config/webpack/loaders.js
+++ config/webpack/loaders.js
@@ -5,60 +5,67 @@ const autoPrefixer = require('autoprefixer')
 const cssRegex = /\.scss$/
 const cssModuleRegex = /\.module\.scss$/
 
-const babelLoader = ({ type = 'legacy', PROD }) => ({
-  test: /\.(js|jsx|mjs)$/,
-  exclude: /node_modules/,
-  use: {
-    loader: 'babel-loader',
-    options: {
-      // babelrc: false,
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            // debug: false,
-            modules: false,
-            useBuiltIns: 'entry',
-            targets: {
-              browsers:
-                type === 'modern'
-                  ? [
-                      // The last two versions of each browser, excluding versions
-                      // that don't support <script type="module">.
-                      'last 2 Chrome versions',
-                      'not Chrome < 60',
-                      'last 2 Safari versions',
-                      'not Safari < 10.1',
-                      'last 2 iOS versions',
-                      'not iOS < 10.3',
-                      'last 2 Firefox versions',
-                      'not Firefox < 54',
-                      'last 2 Edge versions',
-                      'not Edge < 15'
-                    ]
-                  : ['> 1%', 'last 2 versions', 'Firefox ESR']
+const babelLoader = ({ type = 'legacy', PROD }) => {
+  const plugins = [
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-class-properties',
+    'react-loadable/babel'
+  ]
+  // for some reason @babel/plugin-transform-runtime does not start the server so just added this check
+  if (type === 'legacy') {
+    plugins.push('@babel/plugin-transform-runtime')
+  }
+  return {
+    test: /\.(js|jsx|mjs)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        // babelrc: false,
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              // debug: false,
+              modules: false,
+              useBuiltIns: 'entry',
+              targets: {
+                browsers:
+                  type === 'modern'
+                    ? [
+                        // The last two versions of each browser, excluding versions
+                        // that don't support <script type="module">.
+                        'last 2 Chrome versions',
+                        'not Chrome < 60',
+                        'last 2 Safari versions',
+                        'not Safari < 10.1',
+                        'last 2 iOS versions',
+                        'not iOS < 10.3',
+                        'last 2 Firefox versions',
+                        'not Firefox < 54',
+                        'last 2 Edge versions',
+                        'not Edge < 15'
+                      ]
+                    : ['> 1%', 'last 2 versions', 'Firefox ESR']
+              }
             }
-          }
+          ],
+          '@babel/preset-react',
+          [
+            '@emotion/babel-preset-css-prop',
+            {
+              sourceMap: !PROD,
+              autoLabel: !PROD,
+              labelFormat: !PROD ? '[dirname]--[filename]-[local]' : '',
+              cssPropOptimization: true
+            }
+          ]
         ],
-        '@babel/preset-react',
-        [
-          '@emotion/babel-preset-css-prop',
-          {
-            sourceMap: !PROD,
-            autoLabel: !PROD,
-            labelFormat: !PROD ? '[dirname]--[filename]-[local]' : '',
-            cssPropOptimization: true
-          }
-        ]
-      ],
-      plugins: [
-        '@babel/plugin-syntax-dynamic-import',
-        '@babel/plugin-proposal-class-properties',
-        'react-loadable/babel'
-      ]
+        plugins
+      }
     }
   }
-})
+}
 
 const cssLoaderClient = ({ PROD }) => ({
   test: cssRegex,
@@ -135,7 +142,7 @@ const fileLoaderServer = {
 const client = ({ PROD }) => [
   {
     oneOf: [
-      babelLoader({ PROD }),
+      babelLoader({ PROD, type: 'legacy' }),
       cssLoaderClient({ PROD }),
       urlLoaderClient,
       fileLoaderClient
@@ -145,7 +152,7 @@ const client = ({ PROD }) => [
 const server = ({ PROD }) => [
   {
     oneOf: [
-      babelLoader({ PROD }),
+      babelLoader({ PROD, type: 'server' }),
       cssLoaderServer,
       urlLoaderServer,
       fileLoaderServer
