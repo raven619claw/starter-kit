325ea96b47301e23cc700751a60c8d5b4fee23e7
Ravi <raven619claw@gmail.com>
2019-12-27 19:49:53 +0530

    added comments

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,7 @@
-const env = require('dotenv').config().parsed;
+const env = require('dotenv').config().parsed
 
+// these constants should relate to any config which needs to ne dynamic based on env
+// such as endpoints and dev config
 module.exports = {
   ...env,
   PROD: process.env.NODE_ENV === 'production',
@@ -7,4 +9,4 @@ module.exports = {
   WATCH: process.env.WATCH,
   HOT_RELOAD: process.env.HOT_RELOAD,
   INSPECT: process.env.INSPECT
-};
+}
--- src/shared/constants/index.js
+++ src/shared/constants/index.js
@@ -1,3 +1,5 @@
+// these constants are independent of dev env
+// but as they are to be used across the app then they are placed here
 export const TABLET_THRESHOLD = 1024
 export const MOBILE_THRESHOLD = 768
 export const TABLET = 'tablet'
