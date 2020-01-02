b4ec1cb6f3ff49021da8caabe7be1f7dcd65e06c
Ravi <raven619claw@gmail.com>
2020-01-02 18:07:01 +0530

    file rename

--- src/client/DesktopContainers/List/index.js
+++ src/client/DesktopContainers/List/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import './index.scss'
+import './style.scss'
 // eslint-disable-next-line react/prefer-stateless-function
 export default class List extends Component {
   render() {
