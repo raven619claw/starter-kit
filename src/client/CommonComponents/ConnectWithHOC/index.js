6ff803be0f775b2267da67b1e6a9e29490f87c96
Ravi <raven619claw@gmail.com>
2020-01-09 20:09:11 +0530

    delete connectwithHOC

--- src/client/CommonComponents/ConnectWithHOC/index.js
+++
@@ -1,5 +0,0 @@
-import { connect } from 'react-redux'
-import ContainerHOC from 'client/CommonComponents/ContainerHOC'
-
-export default (...args) => Component =>
-  connect(...args)(ContainerHOC(Component))
