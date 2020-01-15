5c0cd16258a283391a236b593cf36553990180dc
Ravi <raven619claw@gmail.com>
2020-01-16 01:50:44 +0530

    added emotion to snap testing

--- src/client/DesktopContainers/List/index.js
+++ src/client/DesktopContainers/List/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import './style.scss'
 import ContainerHOC from 'client/CommonComponents/ContainerHOC'
+import { style } from './style'
 
-const ListContainer = () => <div className="class1">List Desktop</div>
+const ListContainer = () => (
+  <div css={style} className="class1">
+    List Desktop
+  </div>
+)
 
 export default ContainerHOC(ListContainer)
